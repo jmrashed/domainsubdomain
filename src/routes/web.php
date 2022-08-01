0baceff5cf2f40532a497933566a9e843f640ce7
Rashed Zaman <jmrashed@gmail.com>
2022-08-02 03:22:14 +0600

    update requires

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use  Jmrashed\DomainSubdomain\Http\Controllers\DomainController;
 
-Route::get('/domains', [DomainController::class, 'index']);
-Route::get('/domains/{domain}', [DomainController::class, 'show']);
-Route::post('/domains', [DomainController::class, 'store']);
+Route::get('/domains', [DomainController::class, 'index'])->name('domains.index');
+Route::get('/domains', [DomainController::class, 'create'])->name('domains.create');
+Route::get('/domains/{domain}', [DomainController::class, 'show'])->name('domains.show');
+Route::post('/domains', [DomainController::class, 'store'])->name('domains.store');
