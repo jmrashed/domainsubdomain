b57df010463360ab15a561913dddb5241293ceee
Rashed Zaman <jmrashed@gmail.com>
2022-08-02 03:18:21 +0600

    update requires

--- src/Http/Controllers/DomainController.php
+++ src/Http/Controllers/DomainController.php
@@ -10,7 +10,7 @@ class DomainController extends Controller
 
     public function __construct()
     {
-        $this->middleware('domaincheck');
+        $this->middleware('domain');
     }
 
     public function index()
